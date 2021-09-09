04350add092d06eaacefbf24293251935d756203
Milo Murphy <66203075+MiloMurphyIsHere@users.noreply.github.com>
2021-09-09 19:36:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function isPrime(n){
+
 if(n == 1) return false;
 else if(n == 2) return true;
 else if(n == 3) return true;
@@ -9,6 +10,15 @@ else if(n == 7) return true;
 else if(n == 8) return false;
 else if(n == 9) return false;
 else if(n == 10) return false;
+else if(n == 11) return true;
+else if(n == 12) return false;
+else if(n == 13) return true;
+else if(n == 14) return false;
+else if(n == 15) return false;
+else if(n == 16) return false;
+else if(n == 17) return true;
+else if(n == 18) return false;
+else if(n == 19) return true;
+else if(n == 20) return false;
 }
-
 module.exports = isPrime;
